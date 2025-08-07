764a4f427024ef61ecdd97bbcbbe96644d7d4650
Nguyễn Đức Cảnh <nguyenduccanh2398@gmail.com>
2025-08-07 15:59:49 +0700

    fix: update environment variables and Dockerfile to use port 3002

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
