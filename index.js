60e87af69fa5242dbdb393aafb90e6065748de53
Nguyễn Đức Cảnh <nguyenduccanh2398@gmail.com>
2025-08-07 17:12:39 +0700

    fix: update environment and Dockerfile to use consistent port 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
