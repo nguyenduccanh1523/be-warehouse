97a2fbde6ddfd993873d05dc011b230826006dad
Nguyễn Đức Cảnh <nguyenduccanh2398@gmail.com>
2025-08-07 14:32:58 +0700

    fix: change default port from 3000 to 3001 in .env and index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
