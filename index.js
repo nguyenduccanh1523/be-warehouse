cb9147e54f1cd03854105a20e17a61bcefcd26ed
Nguyễn Đức Cảnh <nguyenduccanh2398@gmail.com>
2025-08-07 13:55:00 +0700

    feat: bind server to all network interfaces for accessibility

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
