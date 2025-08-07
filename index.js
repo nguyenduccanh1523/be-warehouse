71a3b8f873df38766bc92a12ee8d21a52acbf1b2
Nguyễn Đức Cảnh <nguyenduccanh2398@gmail.com>
2025-08-07 14:51:50 +0700

    fix: remove explicit binding to '0.0.0.0' in server listen method

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
